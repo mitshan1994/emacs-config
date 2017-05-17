ae6307f75b03427ae4f5a752eb6003d69b989257
Mit Shan <mitshan@163.com>
2017-05-17 09:26:17 +0800

    windows上中文字体设置成功

--- .emacs.d/different_os/windows.el
+++ .emacs.d/different_os/windows.el
@@ -1,13 +1,12 @@
-(custom-set-variables
+;(custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-names-vector
-   ["#2d3743" "#ff4242" "#74af68" "#dbdb95" "#34cae2" "#008b8b" "#00ede1" "#e1e1e0"])
- '(column-number-mode t)
- '(inhibit-startup-screen t)
- '(custom-enabled-themes (quote (tsdh-dark))))
+; '(ansi-color-names-vector
+;   ["#2d3743" "#ff4242" "#74af68" "#dbdb95" "#34cae2" "#008b8b" "#00ede1" "#e1e1e0"])
+; '(column-number-mode t)
+; '(custom-enabled-themes (quote (tsdh-dark))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -15,4 +14,11 @@
  ;; If there is more than one, they won't work right.
  '(default ((t (:family "Consolas" :foundry "outline" :slant normal :weight normal :height 143 :width normal)))))
 
-(when window-system (set-frame-size (selected-frame) 80 25))
+(when window-system (set-frame-size (selected-frame) 80 26))
+
+;; Chinese Font BEGIN
+(dolist (charset '(kana han symbol cjk-misc bopomofo))
+(set-fontset-font (frame-parameter nil 'font)
+charset
+(font-spec :family "SimSun")))
+;; Chinese Font END
